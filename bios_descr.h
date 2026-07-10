b3c764f2fccd9f9246c87588ba37498d2a6f27d6
ogamespec <ogamespec@gmail.com>
2026-07-10 21:20:56 +0300

    cleanup

---
+++ bios_descr.h
@@ -0,0 +1,5 @@
+typedef struct _BIOS_DESCR
+{
+	uint32_t crc;
+	char descr[32];
+} BIOS_DESCR;
--- decompiled/DirectSoundCreate.c
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-// attributes: thunk
-HRESULT __stdcall DirectSoundCreate(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
-{
-  return __imp_DirectSoundCreate(pcGuidDevice, ppDS, pUnkOuter);
-}
