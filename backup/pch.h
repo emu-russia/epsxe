e1d9d7f59519d06ea49fef4b5ea4666e63efbf1d
ogamespec <ogamespec@gmail.com>
2026-07-18 10:24:10 +0300

    minor

--- backup/pch.h
+++ backup/pch.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <Windows.h>
+#include <intrin.h>
 
 #define __noreturn
 
@@ -10,8 +11,17 @@
 
 typedef BYTE _BYTE;
 typedef DWORD _DWORD;
+typedef uint64_t _QWORD;
+typedef uint16_t _WORD;
+typedef BOOLEAN bool;
 
 #define BYTE2(n) (((n)>>16)&0xff)
+
+// Pointer-based variants that can be used on both sides of an assignment (=)
+#define LOWORD(x) (*((uint16_t*)&(x)))
+#define HIWORD(x) (*((uint16_t*)&(x) + 1))
+#define LODWORD(x) (*((uint32_t*)&(x)))
+#define HIDWORD(x) (*((uint32_t*)&(x) + 1))
 
 #include "../zlib-1.1.3/zlib.h"
 #include "../psxdefs.h"
---
+++ input.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef struct _KBD_NAME {
+	char text[13];
+} KBD_NAME;
+
+typedef struct _JOY_NAME {
+	char text[14];
+} JOY_NAME;
